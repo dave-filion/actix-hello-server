aec28a37aa89d3e3a73d13a8751d8b9ffc1bc2b3
Dave Filion <dave.filion@gmail.com>
2020-03-15 23:42:12 -0400

    hot reload

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use actix_web::*;
+use listenfd::ListenFd;
 
 async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Yo Bro")
+    HttpResponse::Ok().body("Yo")
 }
 
 async fn index2() -> impl Responder {
@@ -10,12 +11,22 @@ async fn index2() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    println!("Initializing server...");
+
+    // start listener (for hot reloading in dev)
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
             .route("/again", web::get().to(index2))
-    })
-        .bind("127.0.0.1:8088").unwrap()
-        .run()
-        .await
+    });
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)?
+    } else {
+        server.bind("127.0.0.1:8088")?
+    };
+    println!("Server running!");
+
+    server.run().await
 }
